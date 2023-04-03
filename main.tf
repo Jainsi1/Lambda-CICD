2e8bc13343499ec75d5a0a14ff19f7193006d48a
Jainsi1 <75137939+Jainsi1@users.noreply.github.com>
2023-04-03 23:06:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
-
-
 module "lambda" {
   source = "./module"
   lambda_bucket = "gjygju"
-  lambda_file_handler = "hello-python.lambda_handler"
+  lambda_file_handler = "lambda_function.lambda_handler"
   lambda_function_name = "test-lambda"
   lambda_memory_size = 512
   lambda_runtime = "python3.8"
