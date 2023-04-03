bd5f56bbd0fd0917871b26d3e2b842199bf1c0ea
Jainsi1 <75137939+Jainsi1@users.noreply.github.com>
2023-04-03 18:36:39 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+
+module "lambda" {
+  source = "./module"
+  lambda_bucket = "gjygju"
+  lambda_file_handler = "hello-python.lambda_handler"
+  lambda_function_name = "test-lambda"
+  lambda_memory_size = 512
+  lambda_runtime = "python3.8"
+  env_vars = {
+      name = "first"
+      second = "22"
+    }
+  }
+
