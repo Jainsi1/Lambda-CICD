0da9f1ba715c72347f365bee538b55288e609efa
Jainsi1 <75137939+Jainsi1@users.noreply.github.com>
2023-04-03 18:47:40 +0530

    Update and rename test.py to lambda_function.py

---
+++ module/python/lambda_function.py
@@ -0,0 +1,31 @@
+import boto3
+import yaml
+client = boto3.client('iam')
+def lambda_handler():
+ 
+  with open('data.yaml') as f:
+         data = yaml.load(f,Loader= yaml.FullLoader)
+
+  userlist = data['Users']
+  for users in userlist: 
+      print(users)     
+      response = client.create_user(
+      UserName=users
+       )
+      print(response)
+
+  grouplist = data['GroupAssignment']     
+  for groups in grouplist:
+      print (groups)
+      group = client.create_group(
+      GroupName=groups
+       )
+      print (group)
+      for x in grouplist[groups]:
+
+          usergroup = client.add_user_to_group(
+          GroupName=groups,
+          UserName=x
+          )      
+        
+          print (usergroup)
