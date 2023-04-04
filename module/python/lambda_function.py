3b79b7bdfc458a6e8fb06fb10f8d9c710e732953
Jainsi1 <75137939+Jainsi1@users.noreply.github.com>
2023-04-04 11:47:04 +0530

    Update lambda_function.py

--- module/python/lambda_function.py
+++ module/python/lambda_function.py
@@ -3,29 +3,29 @@ import yaml
 
 def lambda_handler():
  
-  with open('data.yaml') as f:
-         data = yaml.load(f,Loader= yaml.FullLoader)
+    with open('module/python/data.yaml') as f:
+           data = yaml.load(f,Loader= yaml.FullLoader)
 
-  userlist = data['Users']
-  for users in userlist: 
-      print(users)     
-      response = client.create_user(
-      UserName=users
-       )
-      print(response)
+    userlist = data['Users']
+    for users in userlist: 
+        print(users)     
+        response = client.create_user(
+        UserName=users
+         )
+        print(response)
 
-  grouplist = data['GroupAssignment']     
-  for groups in grouplist:
-      print (groups)
-      group = client.create_group(
-      GroupName=groups
-       )
-      print (group)
-      for x in grouplist[groups]:
+    grouplist = data['GroupAssignment']     
+    for groups in grouplist:
+        print (groups)
+        group = client.create_group(
+        GroupName=groups
+         )
+        print (group)
+        for x in grouplist[groups]:
 
-          usergroup = client.add_user_to_group(
-          GroupName=groups,
-          UserName=x
-          )      
+            usergroup = client.add_user_to_group(
+            GroupName=groups,
+            UserName=x
+            )      
         
-          print (usergroup)
+            print (usergroup)
